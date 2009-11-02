4736d20c43320562f24530c389c6447bdcc35bd6
Mark Haskamp <mark.haskamp@pinnsg.com>
2009-11-02 15:55:23 -0500

    got a slider control to work

--- Resources/javascript/userPrefs.js
+++ Resources/javascript/userPrefs.js
@@ -1,10 +1,10 @@
 
-function clickFoo(e) {
-		
-
+function savePreferences(e) {
 		var a = Titanium.UI.createAlertDialog();
-		// a.setMessage('eddie would go');
-		a.setMessage(sw1.value + ", " + sl1.value);
+		a.setMessage($('pomodoroValue').innerHTML + ", " + sw1.value + ", " + sl1.value + ", " + $F('chkFlash'));
 		a.show();
 }
 
+var sw1 = Titanium.UI.createSwitch({id:'switch1', value:true});
+var sl1 = Titanium.UI.createSlider({id:'slider1', min:0, max:1, value:1, width:100});
+var slPomodoro = Titanium.UI.createSlider({ id:'pomodoroMinutesCtl', min:20, max:40, width: 200 });
