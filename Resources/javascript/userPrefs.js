c4ce28052313308a9f59ebecb7cc866de37b4eb5
Mark Haskamp <mark.haskamp@pinnsg.com>
2009-11-02 16:46:48 -0500

    added sliders for short breaks and long breaks. Did some formatting.

--- Resources/javascript/userPrefs.js
+++ Resources/javascript/userPrefs.js
@@ -8,3 +8,5 @@ function savePreferences(e) {
 var sw1 = Titanium.UI.createSwitch({id:'switch1', value:true});
 var sl1 = Titanium.UI.createSlider({id:'slider1', min:0, max:1, value:1, width:100});
 var slPomodoro = Titanium.UI.createSlider({ id:'pomodoroMinutesCtl', min:20, max:40, width: 200 });
+var slShortBreak = Titanium.UI.createSlider({ id:'shortBreakMinutesCtl', min:3, max:10, width: 100 });
+var slLongBreak = Titanium.UI.createSlider({ id:'longBreakMinutesCtl', min:10, max:20, width: 125 });
