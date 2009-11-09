fcc57c385b21d60df7292371348adc9c690a31c9
Mark Haskamp <mark.haskamp@pinnsg.com>
2009-11-09 09:08:32 -0500

    On load of userPrefs.html, current values for sliders are displayed.

--- Resources/javascript/userPrefs.js
+++ Resources/javascript/userPrefs.js
@@ -60,3 +60,4 @@ var slLongBreak = Titanium.UI.createSlider({ id:'longBreakMinutesCtl'
                                              ,value: getStoredValues('longBreakMinutes')
                                              });
 
+
